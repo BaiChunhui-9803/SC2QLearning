9598f9012dce675702199fbfcacdb4e1e46e3d74
bch980311@163.com <bch980311@163.com>
2023-11-08 18:52:08 +0800

    2023-11-08

--- GetGameArix/draw/draw_clusters_result.py
+++ GetGameArix/draw/draw_clusters_result.py
@@ -6,10 +6,11 @@ import seaborn as sns
 import csv
 import random
 from pyecharts import options as opts
-from pyecharts.charts import Scatter, Timeline, Tab
+from pyecharts.charts import Scatter, Timeline, Tab, Grid, Line, Page
 from pyecharts.faker import Faker
 from pyecharts.commons.utils import JsCode
 import json
+
 
 def get_point_boundry(point_list):
     x_min = point_list[0][0]
@@ -30,6 +31,7 @@ def get_point_boundry(point_list):
         if y > y_max:
             y_max = y
     return x_min, x_max, y_min, y_max
+
 
 def drawClustersResult_unit4(path):
     with open(path + 'clusters.csv', 'r') as f:
@@ -58,15 +60,15 @@ def drawClustersResult_unit4(path):
             x_min, x_max, y_min, y_max = get_point_boundry(list4[i][0])
             scatter = (
                 Scatter()
-                .add_xaxis(xaxis_data=x_data)
-                .add_yaxis(
+                    .add_xaxis(xaxis_data=x_data)
+                    .add_yaxis(
                     series_name="",
                     y_axis=y_data,
                     symbol_size=20,
                     label_opts=opts.LabelOpts(is_show=False),
                 )
-                .set_series_opts()
-                .set_global_opts(
+                    .set_series_opts()
+                    .set_global_opts(
                     xaxis_opts=opts.AxisOpts(
                         type_="value",
                         splitline_opts=opts.SplitLineOpts(is_show=True),
@@ -98,7 +100,7 @@ def drawClustersResult_unit4(path):
                     )
                 )
             )
-                # .set_global_opts(title_opts=opts.TitleOpts("某商店{}年营业额".format(i)))
+            # .set_global_opts(title_opts=opts.TitleOpts("某商店{}年营业额".format(i)))
             # )
             tl.add(scatter, "sample{}".format(i))
         tl.add_schema(
@@ -114,6 +116,7 @@ def drawClustersResult_unit4(path):
             # )
         )
         tl.render("drawClustersResult.html")
+
 
 def drawClustersResult_unit8(path):
     with open(path + 'clusters.csv', 'r') as f:
@@ -168,7 +171,8 @@ def drawClustersResult_unit8(path):
                         tooltip_opts=opts.TooltipOpts(is_show=False),
                         title_opts=opts.TitleOpts(
                             title="Cluster Result for {} Units: Sample{}".format(list_idx + 1, i),
-                            subtitle="clu_uniformity: {}, clu_crowding: {}".format(list_all[list_idx][i][1], list_all[list_idx][i][2]),
+                            subtitle="clu_uniformity: {}, clu_crowding: {}".format(list_all[list_idx][i][1],
+                                                                                   list_all[list_idx][i][2]),
                             pos_top='0%',  # 标题的垂直位置
                             pos_left='center',  # 标题的水平位置
                             title_textstyle_opts=opts.TextStyleOpts(
@@ -202,12 +206,15 @@ def drawClustersResult_unit8(path):
 
 
 tag_list = []
+
+
 def update_tag_list(unit_list):
     for unit in unit_list:
         if unit[0] in tag_list:
             pass
         else:
             tag_list.append(unit[0])
+
 
 def drawClustersHealthResult(path):
     tab = Tab()
@@ -250,18 +257,25 @@ def drawClustersHealthResult(path):
                             step_value[cluster_key] = [tuple(map(int, substr.split(','))) for substr in
                                                        lines[cluster_value + 1].strip('\t\n').split(';') if substr]
                             update_tag_list(step_value[cluster_key])
-                figsise = opts.InitOpts(width='650px', height='650px')
+                figsise = opts.InitOpts(width='1550px', height='650px')
                 tl = Timeline(init_opts=figsise)
+                grid_x_list = []
+                grid_y1_list = []
+                grid_y2_list = []
                 for step_key, step_value in game_dict.items():
+                    grid_x_list.append(step_key)
                     x_data = [t[1] for sublist in step_value.values() if isinstance(sublist, list) for t in sublist]
                     y_data = [t[2] for sublist in step_value.values() if isinstance(sublist, list) for t in sublist]
-                    point_data = sorted([(t[0], t[1], t[2]) for sublist in step_value.values() if isinstance(sublist, list) for t in sublist], key=lambda x: x[0])
+                    point_data = sorted(
+                        [(t[0], t[1], t[2]) for sublist in step_value.values() if isinstance(sublist, list) for t in
+                         sublist], key=lambda x: x[0])
                     y_list = []
                     clusters_health_dict = {}
                     for cluster_key, cluster_value in step_value.items():
                         if cluster_key.startswith('c'):
                             clusters_health_dict[cluster_key.strip('c_line')] = round(
-                                sum(item[4] for item in cluster_value) / len(cluster_value) / 255.0 if len(cluster_value) > 0 else 0, 2)
+                                sum(item[4] for item in cluster_value) / len(cluster_value) / 255.0 if len(
+                                    cluster_value) > 0 else 0, 2)
                             for point in cluster_value:
                                 y_list.append([point[2], point[0], point[3], int(cluster_key.strip('c_line'))])
                     x_list = [x[1] for x in point_data]
@@ -295,30 +309,35 @@ def drawClustersHealthResult(path):
                             negative_sum += float(value)
                             negative_count += 1
                     positive_mean = round(positive_sum / positive_count if positive_count > 0 else 0, 2)
+                    grid_y1_list.append(positive_mean)
                     negative_mean = round(negative_sum / negative_count if negative_count > 0 else 0, 2)
+                    grid_y2_list.append(negative_mean)
                     game_health_dict = {'self_units': positive_mean, 'enemy_units': negative_mean}
                     x_min = min(x_data)
                     x_max = max(x_data)
                     y_min = min(y_data)
                     y_max = max(y_data)
+                    grid = Grid()
                     scatter = (
                         Scatter()
                             .add_xaxis(xaxis_data=x_list)
                             .add_yaxis(
                             series_name="",
                             y_axis=new_y_list,
-                            symbol_size=25,
+                            symbol_size=20,
+                            color='green',
                             label_opts=opts.LabelOpts(
+                                is_show=True,
+                                color="blue",
                                 formatter=JsCode(
                                     "function(params){return '('+params.value[0]+','+params.value[1]+'):'+params.value[3];}"
                                 )),
-                            )
-                            .set_series_opts(
-                            itemstyle_opts=opts.ItemStyleOpts(opacity=1)
-                            )
+                        )
+                            .set_series_opts(itemstyle_opts=opts.ItemStyleOpts(opacity=1))
                             .set_global_opts(
                             xaxis_opts=opts.AxisOpts(
                                 type_="value",
+                                name='X Axis',
                                 splitline_opts=opts.SplitLineOpts(is_show=True),
                                 axislabel_opts=opts.LabelOpts(interval=0),
                                 min_=x_min - 1,
@@ -326,6 +345,7 @@ def drawClustersHealthResult(path):
                             ),
                             yaxis_opts=opts.AxisOpts(
                                 type_="value",
+                                name='Y Axis',
                                 axislabel_opts=opts.LabelOpts(interval=0),
                                 splitline_opts=opts.SplitLineOpts(is_show=True),
                                 min_=y_min - 1,
@@ -334,6 +354,7 @@ def drawClustersHealthResult(path):
                             visualmap_opts=opts.VisualMapOpts(
                                 type_="color", max_=len(step_value.keys()) - 2, min_=-1, dimension=4
                             ),
+                            legend_opts=opts.LegendOpts(pos_top="5%", pos_left="5%"),
                             title_opts=opts.TitleOpts(
                                 title=json.dumps(game_health_dict).strip('\{\}').replace('"', ""),
                                 subtitle=json.dumps(clusters_health_dict).strip('\{\}').replace('"', ""),
@@ -351,25 +372,65 @@ def drawClustersHealthResult(path):
                             )
                         )
                     )
-                    tl.add(scatter, "step{}".format(step_key))
+                    line = (
+                        Line()
+                            .add_xaxis(grid_x_list)
+                            .add_yaxis("我方单位", grid_y1_list, is_smooth=True,
+                                       symbol='circle',
+                                       linestyle_opts=opts.LineStyleOpts(color="red"),
+                                       itemstyle_opts=opts.ItemStyleOpts(color="red", color0="red",
+                                                                         border_color="red"),
+                                       label_opts=opts.LabelOpts(is_show=True, color="red")
+                                       # areastyle_opts=opts.AreaStyleOpts(opacity=0.9, color='red')
+                                       )
+                            .add_yaxis("敌方单位", grid_y2_list, is_smooth=True,
+                                       symbol='circle',
+                                       linestyle_opts=opts.LineStyleOpts(color="blue"),
+                                       itemstyle_opts=opts.ItemStyleOpts(color="blue", color0="blue",
+                                                                         border_color="blue"),
+                                       label_opts=opts.LabelOpts(is_show=True, color="blue")
+                                       # areastyle_opts=opts.AreaStyleOpts(opacity=0.9, color='blue')
+                                       )
+                            # .set_series_opts(
+                            # areastyle_opts=opts.AreaStyleOpts(opacity=0.5),
+                            # label_opts=opts.LabelOpts(is_show=True),
+                        # )
+                            .set_global_opts(
+                            xaxis_opts=opts.AxisOpts(
+                                axistick_opts=opts.AxisTickOpts(is_align_with_label=True),
+                                is_scale=False,
+                                boundary_gap=False,
+                            ),
+                            legend_opts=opts.LegendOpts(pos_top="5%", pos_right="5%"),
+                            # visualmap_opts=opts.VisualMapOpts(
+                            #     type_="color", max_=1, min_=1
+                            # ),
+                        )
+                    )
+                    grid.add(scatter, grid_opts=opts.GridOpts(pos_left="5%", pos_right="60%", pos_top="15%"))
+                    grid.add(line,
+                             grid_opts=opts.GridOpts(
+                                 pos_left="45%", pos_right="5%", pos_top="10%", pos_bottom="55%"
+                             )
+                             )
+                    tl.add(grid, "step{}".format(step_key))
                 tl.add_schema(
                     # is_auto_play=True,  # 自动播放
                     play_interval=3000,  # 播放间隔，单位为毫秒
-                    # pos_left='0%',  # Timeline的水平位置
-                    # pos_bottom='5%',  # Timeline的垂直位置
-                    # width='90%',  # Timeline的宽度
+                    pos_left='5%',  # Timeline的水平位置
+                    # pos_bottom='-5%',  # Timeline的垂直位置
+                    width='90%',  # Timeline的宽度
                     # label_opts=opts.LabelOpts(
                     #     is_show=True,  # 是否显示标签
                     #     color='black',  # 标签的颜色
                     #     font_size=12  # 标签的字体大小
                     # )
                 )
+                # page.render("drawClustersHealthResult.html")
                 tab.add(tl, "{}".format(file_id))
     tab.render("drawClustersHealthResult.html")
 
 
-
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     path_TL_MM_8 = './../datas/data_for_render/experiments_datas/two-layer/MM_8/'
     path_TL_MM_8_dist = './../datas/data_for_render/experiments_datas/two-layer/MM_8_dist/'
